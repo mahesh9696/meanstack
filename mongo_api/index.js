4b6cf949d5d0d1b07e30f28329e8728f506f95da
maheshpate <m.mahesh.p@gmail.com>
2019-10-02 19:51:26 +0530

    change file

--- mongo_api/index.js
+++ mongo_api/index.js
@@ -2,11 +2,19 @@ var mongoose = require('mongoose');
 var express = require('express');
 
 var app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // connecting 
-mongoose.connect('mongodb://localhost/playground', {
+mongoose.connect('mongodb://localhost/meanstack', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -18,60 +26,59 @@ mongoose.connect('mongodb://localhost/playground', {
     });
 
 // Schema
-const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean,
-    age: Number
+const employeeSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email_id: String,
+    active: Boolean
 });
 
 // Model
-const Course = mongoose.model('course', courseSchema);
+const Employee = mongoose.model('employee', employeeSchema);
 
 // Save
-app.post('/courses', async function (req, res) {
-    const course = new Course({
-        name: req.body.name,
-        auther: req.body.auther,
-        isPublished: req.body.isPublished,
-        age: req.body.age
+app.post('/employees', async function (req, res) {
+    const employee = new Employee({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email_id: req.body.email_id,
+        active: req.body.active
     });
-    var result = await course.save();
+    var result = await employee.save();
     res.send(result);
 });
 
 // Update
-app.put('/courses/:id', async function (req, res) {
-    const result = await Course.findByIdAndUpdate(req.params.id, { name: req.body.name, age: req.body.age }, {
+app.put('/employees/:id', async function (req, res) {
+    const result = await Employee.findByIdAndUpdate(req.params.id, { first_name: req.body.first_name, last_name: req.body.last_name }, {
         new: true
     });
 
-    if (!result) return res.status(400).send('course not found');
+    if (!result) return res.status(400).send('employee not found');
 
     res.send(result);
 });
 
 // All
-app.get('/courses', async function (req, res) {
-    const result = await Course.find().sort('name');
+app.get('/employees', async function (req, res) {
+    const result = await Employee.find().sort('first_name');
     res.send(result);
 });
 
 // Single
-app.get('/courses/:id', async function (req, res) {
-    const result = await Course.findById(req.params.id);
+app.get('/employees/:id', async function (req, res) {
+    const result = await Employee.findById(req.params.id);
 
-    if (!result) return res.status(400).send('course not found');
+    if (!result) return res.status(400).send('employee not found');
 
     res.send(result);
 });
 
 // Delete
-app.delete('/courses/:id', async function (req, res) {
-    const result = await Course.findByIdAndRemove(req.params.id);
+app.delete('/employees/:id', async function (req, res) {
+    const result = await Employee.findByIdAndRemove(req.params.id);
 
-    if (!result) return res.status(400).send('course not found');
+    if (!result) return res.status(400).send('employee not found');
 
     res.send(result);
 });
